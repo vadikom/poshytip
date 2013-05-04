c2239bf46960095570bd114c6b125ea74b0865e6
Vasil Dinkov <vasko.dinkov@gmail.com>
2013-05-04 11:47:44 +0300

    version 1.2
    - new option 'keepInVieport'
    - new methods 'showDelayed', 'hideDelayed'
    - fix issue #16
    - fix issue #15
    - fix issue #9

--- src/jquery.poshytip.js
+++ src/jquery.poshytip.js
@@ -1,7 +1,7 @@
 /*
- * Poshy Tip jQuery plugin v1.1+
+ * Poshy Tip jQuery plugin v1.2
  * http://vadikom.com/tools/poshy-tip-jquery-plugin-for-stylish-tooltips/
- * Copyright 2010-2011, Vasil Dinkov, http://vadikom.com/
+ * Copyright 2010-2013, Vasil Dinkov, http://vadikom.com/
  */
 
 (function($) {
@@ -57,8 +57,8 @@
 						break;
 					case 'focus':
 						this.$elm.bind({
-							'focus.poshytip': $.proxy(this.show, this),
-							'blur.poshytip': $.proxy(this.hide, this)
+							'focus.poshytip': $.proxy(this.showDelayed, this),
+							'blur.poshytip': $.proxy(this.hideDelayed, this)
 						});
 						break;
 				}
@@ -72,21 +72,21 @@
 			if (this.opts.showOn == 'focus')
 				return true;
 
-			this.clearTimeouts();
-			this.showTimeout = setTimeout($.proxy(this.show, this), this.opts.showTimeout);
+			this.showDelayed();
 		},
 		mouseleave: function(e) {
 			if (this.disabled || this.asyncAnimating && (this.$tip[0] === e.relatedTarget || jQuery.contains(this.$tip[0], e.relatedTarget)))
 				return true;
 
-			var title = this.$elm.data('title.poshytip');
-			if (title !== null)
-				this.$elm.attr('title', title);
+			if (!this.$tip.data('active')) {
+				var title = this.$elm.data('title.poshytip');
+				if (title !== null)
+					this.$elm.attr('title', title);
+			}
 			if (this.opts.showOn == 'focus')
 				return true;
 
-			this.clearTimeouts();
-			this.hideTimeout = setTimeout($.proxy(this.hide, this), this.opts.hideTimeout);
+			this.hideDelayed();
 		},
 		mousemove: function(e) {
 			if (this.disabled)
@@ -107,17 +107,28 @@
 
 			this.reset();
 			this.update();
+
+			// don't proceed if we didn't get any content in update() (e.g. the element has an empty title attribute)
+			if (!this.content)
+				return;
+
 			this.display();
-			if (this.opts.timeOnScreen) {
-				this.clearTimeouts();
-				this.hideTimeout = setTimeout($.proxy(this.hide, this), this.opts.timeOnScreen);
-			}
+			if (this.opts.timeOnScreen)
+				this.hideDelayed(this.opts.timeOnScreen);
+		},
+		showDelayed: function(timeout) {
+			this.clearTimeouts();
+			this.showTimeout = setTimeout($.proxy(this.show, this), typeof timeout == 'number' ? timeout : this.opts.showTimeout);
 		},
 		hide: function() {
 			if (this.disabled || !this.$tip.data('active'))
 				return;
 
 			this.display(true);
+		},
+		hideDelayed: function(timeout) {
+			this.clearTimeouts();
+			this.hideTimeout = setTimeout($.proxy(this.hide, this), typeof timeout == 'number' ? timeout : this.opts.hideTimeout);
 		},
 		reset: function() {
 			this.$tip.queue([]).detach().css('visibility', 'hidden').data('active', false);
@@ -272,6 +283,11 @@
 				this.$tip.css(from).animate(to, this.opts[hide ? 'hideAniDuration' : 'showAniDuration']);
 			}
 			hide ? this.$tip.queue($.proxy(this.reset, this)) : this.$tip.css('visibility', 'inherit');
+			if (active) {
+				var title = this.$elm.data('title.poshytip');
+				if (title !== null)
+					this.$elm.attr('title', title);
+			}
 			this.$tip.data('active', !active);
 		},
 		disable: function() {
@@ -332,21 +348,23 @@
 				case 'right':
 				case 'inner-left':
 					pos.l = xR + this.opts.offsetX;
-					if (pos.l + this.tipOuterW > win.l + win.w)
+					if (this.opts.keepInViewport && pos.l + this.tipOuterW > win.l + win.w)
 						pos.l = win.l + win.w - this.tipOuterW;
 					if (this.opts.alignX == 'right' || this.opts.alignY == 'center')
 						pos.arrow = 'left';
 					break;
 				case 'center':
 					pos.l = xC - Math.floor(this.tipOuterW / 2);
-					if (pos.l + this.tipOuterW > win.l + win.w)
-						pos.l = win.l + win.w - this.tipOuterW;
-					else if (pos.l < win.l)
-						pos.l = win.l;
+					if (this.opts.keepInViewport) {
+						if (pos.l + this.tipOuterW > win.l + win.w)
+							pos.l = win.l + win.w - this.tipOuterW;
+						else if (pos.l < win.l)
+							pos.l = win.l;
+					}
 					break;
 				default: // 'left' || 'inner-right'
 					pos.l = xL - this.tipOuterW - this.opts.offsetX;
-					if (pos.l < win.l)
+					if (this.opts.keepInViewport && pos.l < win.l)
 						pos.l = win.l;
 					if (this.opts.alignX == 'left' || this.opts.alignY == 'center')
 						pos.arrow = 'right';
@@ -358,7 +376,7 @@
 					// 'left' and 'right' need priority for 'target'
 					if (!pos.arrow || this.opts.alignTo == 'cursor')
 						pos.arrow = 'top';
-					if (pos.t + this.tipOuterH > win.t + win.h) {
+					if (this.opts.keepInViewport && pos.t + this.tipOuterH > win.t + win.h) {
 						pos.t = yT - this.tipOuterH - this.opts.offsetY;
 						if (pos.arrow == 'top')
 							pos.arrow = 'bottom';
@@ -366,17 +384,19 @@
 					break;
 				case 'center':
 					pos.t = yC - Math.floor(this.tipOuterH / 2);
-					if (pos.t + this.tipOuterH > win.t + win.h)
-						pos.t = win.t + win.h - this.tipOuterH;
-					else if (pos.t < win.t)
-						pos.t = win.t;
+					if (this.opts.keepInViewport) {
+						if (pos.t + this.tipOuterH > win.t + win.h)
+							pos.t = win.t + win.h - this.tipOuterH;
+						else if (pos.t < win.t)
+							pos.t = win.t;
+					}
 					break;
 				default: // 'top' || 'inner-bottom'
 					pos.t = yT - this.tipOuterH - this.opts.offsetY;
 					// 'left' and 'right' need priority for 'target'
 					if (!pos.arrow || this.opts.alignTo == 'cursor')
 						pos.arrow = 'bottom';
-					if (pos.t < win.t) {
+					if (this.opts.keepInViewport && pos.t < win.t) {
 						pos.t = yB + this.opts.offsetY;
 						if (pos.arrow == 'bottom')
 							pos.arrow = 'top';
@@ -439,7 +459,7 @@
 					handler = function() {
 						var $this = $(this);
 						if (!$this.data('poshytip'))
-							$this.poshytip(deadOpts).poshytip('show');
+							$this.poshytip(deadOpts).poshytip('showDelayed');
 					};
 					this.live ?
 						this.live('focus.poshytip', handler) :
@@ -471,6 +491,7 @@
 							// ('bottom', 'center', 'top', 'inner-bottom', 'inner-top') - 'inner-*' matter if alignTo:'target'
 		offsetX:		-22,		// offset X pixels from the default position - doesn't matter if alignX:'center'
 		offsetY:		18,		// offset Y pixels from the default position - doesn't matter if alignY:'center'
+		keepInViewport:		true,		// reposition the tooltip if needed to make sure it always appears inside the viewport
 		allowTipHover:		true,		// allow hovering the tip without hiding it onmouseout of the target - matters only if showOn:'hover'
 		followCursor:		false,		// if the tip should follow the cursor - matters only if showOn:'hover' and alignTo:'cursor'
 		fade: 			true,		// use fade animation
